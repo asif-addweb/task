c48db1b58ee24014e1f6ea560a32542e832ae395
asif-addweb <128346710+asif-addweb@users.noreply.github.com>
2023-10-08 23:09:52 +0530

    Update task.php

--- task.php
+++ task.php
@@ -1,114 +1,163 @@
 <?php
-// 1)Find the smallest and largest number in an array without using any function.
-$arr = [5, 2, 9, 1, 7, 3];
-$smallest = $arr[3];
-$largest = $arr[3];
+// // 1)Find the smallest and largest number in an array without using any function.
+// $arr = [5, 2, 9, 1, 7, 3];
+// $smallest = $arr[3];
+// $largest = $arr[3];
 
-foreach ($arr as $num) {
-    // Find the smallest number
-    if ($num < $smallest) {
-        $smallest = $num;
-    }
+// foreach ($arr as $num) {
+//     // Find the smallest number
+//     if ($num < $smallest) {
+//         $smallest = $num;
+//     }
 
-    // Find the largest number
-    if ($num > $largest) {
-        $largest = $num;
-    }
-}
+//     // Find the largest number
+//     if ($num > $largest) {
+//         $largest = $num;
+//     }
+// }
 
-echo "Smallest: $smallest\n";
-echo "<br>";
-echo "Largest: $largest\n";
+// echo "Smallest: $smallest\n";
+// echo "<br>";
+// echo "Largest: $largest\n";
 
-echo "<br>";
-echo "<br>";
+// echo "<br>";
+// echo "<br>";
 
 
-// 2)Remove duplicates from an array without using any inbuilt functions?
+// // 2)Remove duplicates from an array without using any inbuilt functions?
 
-// Input array with duplicates
-$arr = [1, 2, 2, 3, 4, 4, 5];
+// // Input array with duplicates
+// $arr = [1, 2, 2, 3, 4, 4, 5];
 
-// Create an empty array to store unique values
-$uniqueArr = [];
+// // Create an empty array to store unique values
+// $uniqueArr = [];
 
-// Iterate through the input array
-foreach ($arr as $num) {
-    $isDuplicate = false;
+// // Iterate through the input array
+// foreach ($arr as $num) {
+//     $isDuplicate = false;
     
-    // Check if the value is already in the unique array
-    foreach ($uniqueArr as $uniqueNum) {
-        if ($num === $uniqueNum) {
-            $isDuplicate = true;
-            break;
+//     // Check if the value is already in the unique array
+//     foreach ($uniqueArr as $uniqueNum) {
+//         if ($num === $uniqueNum) {
+//             $isDuplicate = true;
+//             break;
+//         }
+//     }
+
+//     // If it's not a duplicate, add it to the unique array
+//     if (!$isDuplicate) {
+//         $uniqueArr[] = $num;
+//     }
+// }
+
+// // Output the array without duplicates
+// echo "Array without duplicates: ";
+// foreach ($uniqueArr as $num) {
+//     echo "$num ";
+// }
+
+// echo "<br>";
+// echo "<br>";
+
+
+// // 3)Create html table box based on given input.
+// // Example:2
+// // Input: Number of rows and columns
+// $rows = 2;
+// $columns = 2;
+
+// // Start the HTML table
+// echo "<table border='1'>\n";
+
+// // Outer loop for rows
+// for ($i = 1; $i <= $rows; $i++) {
+//     echo "<tr>\n";
+
+//     // Inner loop for columns
+//     for ($j = 1; $j <= $columns; $j++) {
+//         echo "<td>$j</td>";
+//     }
+
+//     echo "</tr>\n";
+// }
+
+// // Close the HTML table
+// echo "</table>\n";
+
+// echo "<br>";
+// echo "<br>";
+
+
+// //  Create html table box based on given input.
+// // Example:3
+// // Input: Number of rows and columns
+// $rows = 3;
+// $columns = 3;
+
+// // Start the HTML table
+// echo "<table border='1'>\n";
+
+// // Outer loop for rows
+// for ($i = 1; $i <= $rows; $i++) {
+//     echo "<tr>\n";
+
+//     // Inner loop for columns
+//     for ($j = 1; $j <= $columns; $j++) {
+//         echo "<td>$j</td>";
+//     }
+
+//     echo "</tr>\n";
+// }
+
+// // Close the HTML table
+// echo "</table>\n";
+
+class BankAccount {
+    public $accountNumber;
+    public $balance;
+
+
+    // Constructor to initialize the account
+    public function __construct($accountNumber, $initialBalance) {        
+        $this->accountNumber = $accountNumber;
+        $this->balance = $initialBalance;
+    }
+    // Method to deposit money into the account
+    public function deposit($amount) {
+        if ($amount > 0) {
+            $this->balance += $amount;
+            echo "Deposited $amount into account $this->accountNumber. New balance: $this->balance\n";
+        } else {
+            echo "Invalid deposit amount. Please enter a positive amount.\n";
         }
     }
 
-    // If it's not a duplicate, add it to the unique array
-    if (!$isDuplicate) {
-        $uniqueArr[] = $num;
+    // Method to withdraw money from the account
+    public function withdraw($amount) {
+        if ($amount > 0 && $amount <= $this->balance) {
+            $this->balance -= $amount;
+            echo "Withdrawn $amount from account $this->accountNumber. New balance: $this->balance\n";
+        } elseif ($amount <= 0) {
+            echo "Invalid withdrawal amount. Please enter a positive amount.\n";
+        } else {
+            echo "Insufficient funds. Cannot withdraw $amount from account $this->accountNumber. Current balance: $this->balance\n";
+        }
+    }
+
+    // Method to get the current balance
+    public function getBalance() {
+        return $this->balance;
     }
 }
 
-// Output the array without duplicates
-echo "Array without duplicates: ";
-foreach ($uniqueArr as $num) {
-    echo "$num ";
-}
+// Create a new BankAccount instance
+$account1 = new BankAccount("123456789", 1000);
 
-echo "<br>";
-echo "<br>";
+// Deposit and withdraw money
+$account1->deposit(500);
+$account1->withdraw(200);
+$account1->withdraw(800); // This will result in insufficient funds
 
-
-// 3)Create html table box based on given input.
-// Example:2
-// Input: Number of rows and columns
-$rows = 2;
-$columns = 2;
-
-// Start the HTML table
-echo "<table border='1'>\n";
-
-// Outer loop for rows
-for ($i = 1; $i <= $rows; $i++) {
-    echo "<tr>\n";
-
-    // Inner loop for columns
-    for ($j = 1; $j <= $columns; $j++) {
-        echo "<td>$j</td>";
-    }
-
-    echo "</tr>\n";
-}
-
-// Close the HTML table
-echo "</table>\n";
-
-echo "<br>";
-echo "<br>";
-
-
-//  Create html table box based on given input.
-// Example:3
-// Input: Number of rows and columns
-$rows = 3;
-$columns = 3;
-
-// Start the HTML table
-echo "<table border='1'>\n";
-
-// Outer loop for rows
-for ($i = 1; $i <= $rows; $i++) {
-    echo "<tr>\n";
-
-    // Inner loop for columns
-    for ($j = 1; $j <= $columns; $j++) {
-        echo "<td>$j</td>";
-    }
-
-    echo "</tr>\n";
-}
-
-// Close the HTML table
-echo "</table>\n";
-
+// Get the current balance
+$balance = $account1->getBalance();
+echo "Current balance for account $account1->accountNumber: $balance\n";
